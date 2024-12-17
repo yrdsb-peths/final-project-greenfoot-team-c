34179bb47b291e6d4d613515afaf70bbbcfe24bd
JasonLeung1 <440004783@gapps.yrdsb.ca>
2024-12-18 01:46:40 +0800

    Made a wave setting
    The button picture not added yet. Considering to draw or find on the internet.

--- MyWorld.java
+++ MyWorld.java
@@ -1,7 +1,17 @@
 import greenfoot.*;
+import java.util.Queue;
+import java.util.LinkedList;
 
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+
+        
+        addObject(new Button(this::start, "Start the game"), 300, 310);
+    }
+    
+    public void start() {
+        Greenfoot.setWorld(new FirstLevel());
+        
     }
 }
