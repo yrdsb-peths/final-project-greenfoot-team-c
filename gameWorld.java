0ca1ef999e516480e35222480a5b776157817741
JasonLeung1 <440004783@gapps.yrdsb.ca>
2025-01-08 02:31:28 +0800

    Added Image
    The background picture...????

--- gameWorld.java
+++ gameWorld.java
@@ -16,7 +16,8 @@ public class gameWorld extends World
     public gameWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(600, 400, 1);
+        //setBackground("Background.png");
         
         Robot robot = new Robot();        
         addObject(robot, 300, 200);
